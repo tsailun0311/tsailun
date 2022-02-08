ef10817dd6fb914bf1cf5ceb5d965608976b6019
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2022-02-08 14:27:21 +0800

    15

---
+++ DemonSlayer/double.js
@@ -0,0 +1,11 @@
+document.write('<hr width="700" class="index1" /><div align="center"><p>&nbsp;</p><p class="indexlist">')
+var oldURL = document.referrer;
+var url = location.href;
+if (url.indexOf('?') != -1) {
+  var ary1 = url.split('?');
+  var ary2 = ary1[1].split('&');
+  var ary3 = ary2[0].split('=');
+  var id = ary3[1];
+}
+document.write('<a href="' + oldURL + '#' + id + '"><span class="tpye2">回上一頁</span></a>');
+document.write('</p><p>&nbsp;</p></div><p>&nbsp;</p>');
