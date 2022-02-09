8fc9e00dbcdbd4c2199a260919cec8fe59ed5113
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2022-02-09 19:04:41 +0800

    2

--- DemonSlayer/double.js
+++ DemonSlayer/double.js
@@ -1,4 +1,3 @@
-document.write('<hr width="700" class="index1" /><div align="center"><p>&nbsp;</p><p class="indexlist">')
 var oldURL = document.referrer;
 var url = location.href;
 if (url.indexOf('?') != -1) {
@@ -7,5 +6,10 @@ if (url.indexOf('?') != -1) {
   var ary3 = ary2[0].split('=');
   var id = ary3[1];
 }
-document.write('<a href="' + oldURL + '#' + id + '"><span class="tpye2">回上一頁</span></a>');
+document.write('<hr width="700" class="index1" /><div align="center"><p>&nbsp;</p><p class="indexlist">')
+if (id == "map") {
+  document.write('<a href="https://www.tsailun.tk"><span class="tpye2">回首頁</span></a>');
+} else {
+  document.write('<a href="' + oldURL + '#' + id + '"><span class="tpye2">回上一頁</span></a>');
+}
 document.write('</p><p>&nbsp;</p></div><p>&nbsp;</p>');
