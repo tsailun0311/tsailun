189a72179054e5fa6cae1b6d2e252c16e0cedc1b
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2022-05-22 17:23:06 +0800

    101112

--- cube/header.js
+++ cube/header.js
@@ -1,11 +1,11 @@
 document.write( /* image map by http://www.image-map.net/ */
-  '<img src="../headimg.jpg" width="750">'+
+  '<img src="headimg.jpg" width="750">'+
   '<div id="tabs10">' +
   '<ul>' +
   '<li><span class="indexlist"><a href="https://www.tsailun.tk/index.html" title="首頁">首頁</a></span></li>' +
-  '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/f2l/f2l01.html" title="f2l">f2l</a></span></li>' +
-  '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/f2l/f2l01.html" title="oll">oll</a></span></li>' +
-  '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/f2l/f2l01.html" title="pll">pll</a></span></li>'
+  '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/f2l.html" title="F2L">F2L</a></span></li>' +
+  '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/f2l.html" title="OLL">OLL</a></span></li>' +
+  '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/f2l.html" title="PLL">PLL</a></span></li>'
 );
 document.write(
   '</ul></div>'
