7711172514139483ed2ebee502e782b399d9a7f8
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2022-06-22 09:44:51 +0800

    30

--- cube/header.js
+++ cube/header.js
@@ -4,7 +4,7 @@ document.write(
   '<ul>' +
   '<li><span class="indexlist"><a href="https://www.tsailun.tk/index.html" title="首頁">首頁</a></span></li>' +
   '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/f2l.html" title="F2L">F2L</a></span></li>' +
-  '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/f2l.html" title="OLL">OLL</a></span></li>' +
+  '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/oll.html" title="OLL">OLL</a></span></li>' +
   '<li><span class="indexlist"><a href="https://www.tsailun.tk/cube/pll.html" title="PLL">PLL</a></span></li>'
 );
 document.write(
