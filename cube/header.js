8ad499e281d9b6886a0d724e71b7b3af23b66d5c
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2022-07-01 20:47:11 +0800

    46

--- cube/header.js
+++ cube/header.js
@@ -5,6 +5,7 @@ document.write(
   '<li class="yello"><a href="https://www.tsailun.tk/index.html" title="首頁"><span class="indexlist">首頁</span></a></li>' +
   '<li class="yello"><a href="https://www.tsailun.tk/man.html" title="鬼滅之刃"><span class="indexlist">鬼滅之刃</span></a></li>'+
   '<li><a href="https://www.tsailun.tk/cube/f2l.html" title="F2L"><span class="indexlist">F2L</span></a></li>' +
+  '<li><a href="https://www.tsailun.tk/cube/bestf2l.html" title="F2L非標"><span class="indexlist">F2L非標</span></a></li>' +
   '<li><a href="https://www.tsailun.tk/cube/oll.html" title="OLL"><span class="indexlist">OLL</span></a></li>' +
   '<li><a href="https://www.tsailun.tk/cube/pll.html" title="PLL"><span class="indexlist">PLL</span></a></li>'
 );
