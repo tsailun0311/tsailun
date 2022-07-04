ec083615c65fb1ee5ecf098f5425323cbb2c047a
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2022-07-04 23:03:43 +0800

    53

--- cube/header.js
+++ cube/header.js
@@ -2,13 +2,13 @@ document.write(
   '<img src="headimg.jpg" width="736"  style="border-style:double;border-width:7px;border-color:rgb(229, 229, 105);">'+
   '<div id="tabs10">' +
   '<ul>' +
-  '<li class="yello"><a href="https://www.tsailun.tk/index.html" title="首頁"><span class="indexlist">首頁</span></a></li>' +
-  '<li class="yello"><a href="https://www.tsailun.tk/man.html" title="鬼滅之刃"><span class="indexlist">鬼滅之刃</span></a></li>'+
-  '<li><a href="https://www.tsailun.tk/cube/cross.html" title="CROSS"><span class="indexlist">CROSS</span></a></li>' +
-  '<li><a href="https://www.tsailun.tk/cube/f2l.html" title="F2L"><span class="indexlist">F2L</span></a></li>' +
-  '<li><a href="https://www.tsailun.tk/cube/bestf2l.html" title="F2L非標"><span class="indexlist">F2L非標</span></a></li>' +
-  '<li><a href="https://www.tsailun.tk/cube/oll.html" title="OLL"><span class="indexlist">OLL</span></a></li>' +
-  '<li><a href="https://www.tsailun.tk/cube/pll.html" title="PLL"><span class="indexlist">PLL</span></a></li>'
+  '<li class="g1"><a href="index.html" ><span class="indexlist">首頁</span></a></li>' +
+  '<li class="g1"><a href="man.html" ><span class="indexlist">鬼滅之刃</span></a></li>'+
+  '<li class="g2"><a href="cube/cross.html" title="CROSS"><span class="indexlist">CROSS</span></a></li>' +
+  '<li class="g2"><a href="cube/f2l.html" title="F2L"><span class="indexlist">F2L</span></a></li>' +
+  '<li class="g2"><a href="cube/bestf2l.html" title="F2L非標"><span class="indexlist">F2L非標</span></a></li>' +
+  '<li class="g2"><a href="cube/oll.html" title="OLL"><span class="indexlist">OLL</span></a></li>' +
+  '<li class="g2"><a href="cube/pll.html" title="PLL"><span class="indexlist">PLL</span></a></li>'
 );
 document.write(
   '</ul></div>'+
