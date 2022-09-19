5a4b655947a41d2b09b0072fb1420783bc6e7850
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2022-09-19 17:59:24 +0800

    70

--- DATA/footer.js
+++ DATA/footer.js
@@ -2,23 +2,20 @@ if (!i2yesCounter) {
   var i2yesCounter = {
     render: function Get(g) {
       var width = (g.s) ? 'width=' + g.s : '';
-      var Tar = 'target=_blank';
-      var f = '<a ' + Tar + ' href="http://' + g.y + '.i2yes.com/">';
-      f += '<img ' + width + ' border=0 src="http://' + g.y + '.i2yes.com/' + g.y + '.php?p=' + g.v + '_' + g.p + '&v=' + g.v + '&d=' + g.d + '&r=' + g.r + '&t=' + g.t + '&n=' + g.n + '" title="FREE Counter / i2yes.com" alt="FREE Counter / i2yes.com">';
-      f += '</a>';
+      var f = '<img ' + width + ' border=0 src="http://' + g.y + '.i2yes.com/' + g.y + '.php?p=' + g.v + '_' + g.p + '&v=' + g.v + '&d=' + g.d + '&r=' + g.r + '&t=' + g.t + '&n=' + g.n + '" title="FREE Counter / i2yes.com" alt="FREE Counter / i2yes.com">';
       return f;
-    }
+    }//https://counter.i2yes.com/counter.php?p=www.tsailun.tk_tsailuncounter&v=www.tsailun.tk&d=4&r=0&t=font148&n=345
   };
 };
 document.write('<p><span class="indexlist">　觀光人數&nbsp;&nbsp;');
 document.write(i2yesCounter.render({
   y: 'counter',
-  p: 'tsailuncounter', //改成您專有的計數器名稱,注意別跟別人一樣
-  v: 'www.tsailun.tk', //您的網址,不對的話無法使用,也可以用沒有 www 的網址,如 i2yes.com
+  p: 'tsailuncounter', //專有的計數器名稱
+  v: 'www.tsailun.tk', //網址
   d: 4, //數字位數
-  r: 0, //1=不接受Reload,0=Reload會+1
-  t: 'font148', //字型 font001 - font156 可用
-  s: 45, //指定大小,只能輸入數字例;100, 0為不指定尺寸(原寸)
-  n: 345 //指定起始的數字
+  r: 0, //1=不Reload,0=Reload會+1
+  t: 'font148', //字型font001-font156
+  s: 45, //大小,0為不指定尺寸
+  n: 345 //起始的數字
 }));
 document.write('</span></p><p>&nbsp;</p>');
