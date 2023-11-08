ae4373fcaaf1b2bacb3b6fc824193664d1ad4acb
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2023-11-08 20:45:42 +0800

    delete DS

--- DATA/cfopheader.js
+++ DATA/cfopheader.js
@@ -3,16 +3,10 @@ document.write(
   '<div id="tabs10">' +
   '<ul>' +
   '<li class="g1"><a href="index.html" ><span class="indexlist">首頁</span></a></li>' +
-  '<li class="g1"><a href="man.html" ><span class="indexlist">鬼滅之刃</span></a></li>' +
   '<li class="g2"><a href="lbl.html" ><span class="indexlist">LBL</span></a></li>' +
   '<li class="g3"><a href="cross.html"><span class="indexlist">CROSS</span></a></li>' +
   '<li class="g3"><a href="f2l.html"><span class="indexlist">F2L</span></a></li>' +
-  '<li class="g3"><a href="bestf2l.html"><span class="indexlist">F2L非標</span></a></li>' +
   '<li class="g3"><a href="oll.html"><span class="indexlist">OLL</span></a></li>' +
   '<li class="g3"><a href="pll.html"><span class="indexlist">PLL</span></a></li>'+
-  '<li class="g3"><a href="pll2.html"><span class="indexlist">六格觀察法</span></a></li>'
-);
-document.write(
-  '</ul></div>' +
-  '<script src="DATA/mouselove.js"></script>'
+  '<li class="g3"><a href="pll2.html"><span class="indexlist">六格觀察法</span></a></li></div>'
 );
