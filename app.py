134334aeec6bad6990349f3ef007322f452a48d1
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2024-01-20 20:01:39 +0800

    2

--- app.py
+++ app.py
@@ -3,18 +3,8 @@ from flask import Flask, render_template, request
 from json import loads, dumps
 
 app = Flask(__name__)
-title = ["蔡倫魔方", "LBL", "CFOP CROSS", "CFOP F2L",
+title = ["蔡倫魔方", "花式魔方", "LBL", "CFOP CROSS", "CFOP F2L",
          "CFOP OLL", "CFOP PLL", "CFOP 六格觀察法", "找不到頁面!!"]
-
-
-@app.route('/cross', methods=['GET'])
-def cross():
-    return render_template('cross.html', title=title[2])
-
-
-@app.route('/f2l', methods=['GET'])
-def f2l():
-    return render_template('f2l.html', title=title[3])
 
 
 @app.route('/', methods=['GET'])
@@ -35,29 +25,44 @@ def index():
     return render_template('index.html', title=title[0], people=str(people[1]))
 
 
+@app.route('/special', methods=['GET'])
+def special():
+    return render_template('special.html', title=title[1])
+
+
 @app.route('/lbl', methods=['GET'])
 def lbl():
-    return render_template('lbl.html', title=title[1])
+    return render_template('lbl.html', title=title[2])
+
+
+@app.route('/cross', methods=['GET'])
+def cross():
+    return render_template('cross.html', title=title[3])
+
+
+@app.route('/f2l', methods=['GET'])
+def f2l():
+    return render_template('f2l.html', title=title[4])
 
 
 @app.route('/oll', methods=['GET'])
 def oll():
-    return render_template('oll.html', title=title[4])
+    return render_template('oll.html', title=title[5])
 
 
 @app.route('/pll', methods=['GET'])
 def pll():
-    return render_template('pll.html', title=title[5])
+    return render_template('pll.html', title=title[6])
 
 
 @app.route('/pll2', methods=['GET'])
 def pll2():
-    return render_template('pll2.html', title=title[6])
+    return render_template('pll2.html', title=title[7])
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html', title=title[7]), 404
+    return render_template('404.html', title=title[8]), 404
 
 
 if __name__ == '__main__':
