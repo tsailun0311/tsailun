a8237a035c81fd6583a27f8efc80fedb4ce41a9d
tsailun0311 <91107054+tsailun0311@users.noreply.github.com>
2024-10-22 20:31:09 +0800

    pll2

--- app.py
+++ app.py
@@ -1,40 +1,9 @@
 from __future__ import division, print_function
-from flask import Flask, Response, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 title = ["蔡倫魔方", "花式魔方", "LBL", "CFOP CROSS", "CFOP F2L",
-         "CFOP OLL", "CFOP PLL", "找不到頁面!!"]
-color_mapping = {
-    'b': '#07f',
-    'o': 'orange',
-    'r': 'red',
-    'g': '#0d0',
-}
-
-
-def generate_rubiks_cube_svg(cube_colors):
-    colors = [color_mapping.get(
-        cube_colors[i], 'white') for i in range(6)]
-    svg_content = f'''
-    <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="-1 -1  122 122">
-        <polygon points="0,50 20,40 40,50 20,60" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="20,40 40,30 60,40 40,50" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="20,60 40,50 60,60 40,70" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="40,30 60,20 80,30 60,40" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="40,50 60,40 80,50 60,60" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="40,70 60,60 80,70 60,80" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="60,40 80,30 100,40 80,50" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="60,60 80,50 100,60 80,70" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="80,50 100,40 120,50 100,60" fill="yellow" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="0,50 20,60 20,80 0,70" fill="{colors[0]}" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="20,60 40,70 40,90 20,80" fill="{colors[1]}" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="40,70 60,80 60,100 40,90" fill="{colors[2]}" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="60,80 80,70 80,90 60,100" fill="{colors[3]}" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="80,70 100,60 100,80 80,90" fill="{colors[4]}" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-        <polygon points="100,60 120,50 120,70 100,80" fill="{colors[5]}" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-    </svg>
-    '''
-    return svg_content
+         "CFOP OLL", "CFOP PLL", "找不到頁面!!", "其它"]
 
 
 @app.route('/', methods=['GET'])
@@ -44,7 +13,7 @@ def index():
 
 @app.route('/other', methods=['GET'])
 def other():
-    return render_template('other.html', title=title[0])
+    return render_template('other.html', title=title[8])
 
 
 @app.route('/special', methods=['GET'])
@@ -75,17 +44,6 @@ def oll():
 @app.route('/pll', methods=['GET'])
 def pll():
     return render_template('pll.html', title=title[6])
-
-
-@app.route('/pll2', methods=['GET'])
-def pll2():
-    return render_template('pll2.html', title=title[7])
-
-
-@app.route('/<cube_colors>.svg')
-def cube_svg(cube_colors):
-    svg_data = generate_rubiks_cube_svg(cube_colors)
-    return Response(svg_data, mimetype='image/svg+xml')
 
 
 @app.errorhandler(404)
